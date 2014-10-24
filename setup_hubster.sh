bb18366792c72110ce03b3b7598c229e82c819a7
maartenbeekman <maartenbeekman@users.noreply.github.com>
2014-10-24 19:30:01 +0200

    Create setup_hubster.sh

---
+++ setup_hubster.sh
@@ -0,0 +1,25 @@
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get autoremove
+
+# sudo apt-get install apache2
+# sudo apt-get install php5 libapache2-mod-php5 -y
+
+# sudo apt-get install gpsd, libgps20 and python-gps
+# add user pi to dialout group
+# sudo modprobe ipv6
+# wget xxxx.gps
+# wget kplex
+# ls /dev/pts
+# gpsfake -p xxx.gps
+# ls /dev/pts
+# nano /etc/kplex.conf, uncomment lines and change filename to /dev/pts/x
+# sudo apt-get install nmap
+
+# http://www.instructables.com/id/Turn-your-Raspberry-Pi-into-a-Portable-Bluetooth-A/step3/Install-prerequisite-packages/
+
+# http://computers.tutsplus.com/articles/using-a-raspberry-pi-as-an-airplay-receiver--mac-54316
+
+# nmea0183-signalk?
+# Install Kplex?
